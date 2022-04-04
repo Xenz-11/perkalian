ad3415e3faf6aa623b440e0243f3f8f5d45ca278
Inu Gans <102963507+Bang-Inu@users.noreply.github.com>
2022-04-05 01:36:32 +0700

    Create jumlah.py

---
+++ module/jumlah.py
@@ -0,0 +1,13 @@
+## kode penjumlahan
+
+# Memasukkan Inputan Angka
+angka1 = input('Tulis angka pertama: ')
+angka2 = input('Tulis angka kedua: ')
+
+# Mengkonversi Angka lalu Menjumlahkannya
+sum = int(angka1) + int(angka2)
+
+# Menampilkan Hasil Penjumlahan
+print('{0} + {1} = {2}'.format(angka1, angka2, sum))
+print ('mknya pinter bego')
+
